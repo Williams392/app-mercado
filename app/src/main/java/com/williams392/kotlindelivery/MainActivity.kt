a9ca429f386d23f111328b5934ceaa130c693462
Williams392 <williamsvalle392@gmail.com>
2024-09-09 14:52:40 -0500

    first commit

---
+++ app/src/main/java/com/williams392/kotlindelivery/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.williams392.kotlindelivery
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
+
+/*
+
+1. Se agrego 2 fuentes
+    _ de: https://www.1001freefonts.com/
+
+ */
